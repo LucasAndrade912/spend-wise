18eadb919548e61fa49a215509ad7477b2beacf1
Lucas Andrade <lucasantos.dev@gmail.com>
2025-05-19 23:39:56 -0300

    feat: create SignUp page

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,28 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 import { SignUp } from './pages/SignUp';
 import { SignIn } from './pages/SignIn';
 
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
+
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path="sign-up" element={<SignUp />} />
-                <Route path="sign-in" element={<SignIn />} />
-            </Routes>
-        </BrowserRouter>
+        <CssBaseline />
+        <ThemeProvider theme={darkTheme}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="sign-up" element={<SignUp />} />
+                    <Route path="sign-in" element={<SignIn />} />
+                </Routes>
+            </BrowserRouter>
+        </ThemeProvider>
     </StrictMode>
 );
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,11 +1,142 @@
-import { Typography } from '@mui/material';
+import { Box, Button, InputAdornment, TextField, Typography } from '@mui/material';
+import { lightBlue } from '@mui/material/colors';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import Drafts from '@mui/icons-material/Drafts';
+import Lock from '@mui/icons-material/Lock';
+
+import authIllustration from '../assets/auth-illustration.svg';
 
 export function SignUp() {
     return (
-        <>
-            <Typography variant="h5" component="h2">
-                SingUp
-            </Typography>
-        </>
+        <Box
+            component="main"
+            sx={{
+                bgcolor: 'grey.900',
+                color: 'white',
+                height: '100vh',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                gap: '224px',
+            }}>
+            <Box
+                component="img"
+                src={authIllustration}
+                alt="Figura ilustrativa"
+                sx={{
+                    width: { lg: '480px', xl: 'auto' },
+                }}
+            />
+
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}>
+                <Typography variant="h5" component="h2">
+                    Bem-vindo ao{' '}
+                    <Typography
+                        component="span"
+                        variant="h5"
+                        sx={{ color: lightBlue[700] }}>
+                        SpenWise
+                    </Typography>
+                </Typography>
+
+                <Box component="form" sx={{ width: '380px', mt: 6 }}>
+                    <TextField
+                        id="fullName"
+                        label="Digite seu nome completo"
+                        type="text"
+                        variant="filled"
+                        sx={{ width: '100%' }}
+                        slotProps={{
+                            input: {
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <AccountCircle />
+                                    </InputAdornment>
+                                ),
+                            },
+                        }}
+                    />
+
+                    <TextField
+                        id="email"
+                        label="Digite seu email"
+                        type="email"
+                        variant="filled"
+                        sx={{ width: '100%', mt: 4 }}
+                        slotProps={{
+                            input: {
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <Drafts />
+                                    </InputAdornment>
+                                ),
+                            },
+                        }}
+                    />
+
+                    <TextField
+                        id="password"
+                        label="Mínimo 8 caracteres"
+                        type="password"
+                        variant="filled"
+                        sx={{ width: '100%', mt: 4 }}
+                        slotProps={{
+                            input: {
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <Lock />
+                                    </InputAdornment>
+                                ),
+                            },
+                        }}
+                    />
+
+                    <TextField
+                        id="confirmPassword"
+                        label="Mínimo 8 caracteres"
+                        type="password"
+                        variant="filled"
+                        sx={{ width: '100%', mt: 4 }}
+                        slotProps={{
+                            input: {
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <Lock />
+                                    </InputAdornment>
+                                ),
+                            },
+                        }}
+                    />
+
+                    <Button
+                        variant="contained"
+                        size="large"
+                        sx={{
+                            width: '100%',
+                            bgcolor: lightBlue[700],
+                            mt: 7,
+                            color: 'white',
+                        }}>
+                        Cadastrar
+                    </Button>
+
+                    <Typography variant="body2" sx={{ textAlign: 'center', mt: 5 }}>
+                        Já está cadastrado?{' '}
+                        <Typography
+                            component="a"
+                            href="/sign-in"
+                            color={lightBlue[700]}
+                            sx={{ textDecoration: 'underline' }}>
+                            Faça login
+                        </Typography>
+                    </Typography>
+                </Box>
+            </Box>
+        </Box>
     );
 }
